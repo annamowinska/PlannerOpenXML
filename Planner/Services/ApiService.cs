c237d401c94fb43cbf7134187660e301e585c4ca
Anna Em <mowinska.ania@gmail.com>
2024-04-10 17:02:39 +0200

    some corrects

--- Planner/Services/ApiService.cs
+++ Planner/Services/ApiService.cs
@@ -12,15 +12,6 @@ namespace Planner.Services
         {
             _httpClient = new HttpClient();
         }
-
-        //public async Task<IEnumerable<Holiday>> GetHolidaysAsync(int year, string countryCode)
-        //{
-        //    var response = await _httpClient.GetAsync($"https://date.nager.at/api/v3/PublicHolidays/{year}/{countryCode}");
-        //    response.EnsureSuccessStatusCode();
-        //    var json = await response.Content.ReadAsStringAsync();
-        //    return JsonConvert.DeserializeObject<IEnumerable<Holiday>>(json);
-        //}
-
         public async Task<IEnumerable<Holiday>> GetHolidaysAsync(int year, params string[] countryCodes)
 {
     List<Holiday> allHolidays = new List<Holiday>();
@@ -37,17 +28,14 @@ namespace Planner.Services
         }
         catch (HttpRequestException ex)
         {
-            // Obsługa błędu zapytania HTTP
             Console.WriteLine($"An error occurred while fetching holidays for {countryCode}: {ex.Message}");
         }
         catch (JsonException ex)
         {
-            // Obsługa błędu deserializacji JSON
             Console.WriteLine($"An error occurred while deserializing holidays for {countryCode}: {ex.Message}");
         }
         catch (Exception ex)
         {
-            // Obsługa innych wyjątków
             Console.WriteLine($"An error occurred: {ex.Message}");
         }
     }
