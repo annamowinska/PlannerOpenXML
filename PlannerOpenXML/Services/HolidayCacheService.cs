652c6e93d298602c29fa63aa3d2460f6edd28815
Anna Em <mowinska.ania@gmail.com>
2024-04-26 14:22:04 +0200

    holiday list in holiday.json without SOLID

---
+++ PlannerOpenXML/Services/HolidayCacheService.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using PlannerOpenXML.Model;
+using System.IO;
+
+namespace PlannerOpenXML.Services;
+
+public class HolidayCacheService
+{
+    private readonly string m_FilePath;
+
+    public HolidayCacheService(string filePath)
+    {
+        m_FilePath = filePath;
+    }
+
+    public async Task SaveHolidaysFromApiToFile(IApiService apiService)
+    {
+        var allHolidays = new List<Holiday>();
+
+        var fromYear = 2023;
+        var toYear = 2033;
+
+        var countryCodes = new List<string> { "DE", "HU" };
+
+        foreach (var countryCode in countryCodes)
+        {
+            for (var year = fromYear; year <= toYear; year++)
+            {
+                var fetchedHolidays = await apiService.GetHolidaysAsync(year, countryCode);
+                allHolidays.AddRange(fetchedHolidays);
+            }
+        }
+
+        SaveHolidaysToFile(allHolidays);
+    }
+
+    public void SaveHolidaysToFile(IEnumerable<Holiday> holidays)
+    {
+        try
+            {
+                var json = JsonConvert.SerializeObject(holidays);
+                File.WriteAllText(m_FilePath, json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while saving holidays to file: {ex.Message}");
+                throw;
+            }
+    }
+
+    public IEnumerable<Holiday> ReadHolidaysFromFile()
+    {
+        try
+            {
+                if (File.Exists(m_FilePath))
+                {
+                    var json = File.ReadAllText(m_FilePath);
+                    return JsonConvert.DeserializeObject<IEnumerable<Holiday>>(json);
+                }
+                else
+                {
+                    return new List<Holiday>();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while reading holidays from file: {ex.Message}");
+                throw;
+            }
+    }
+}
--- PlannerOpenXML/ViewModel/MainViewModel.cs
+++ PlannerOpenXML/ViewModel/MainViewModel.cs
@@ -2,6 +2,7 @@
 using CommunityToolkit.Mvvm.Input;
 using PlannerOpenXML.Model;
 using PlannerOpenXML.Services;
+using System.IO;
 using System.ComponentModel;
 using System.Windows;
 
@@ -10,16 +11,18 @@ namespace PlannerOpenXML.ViewModel;
 public partial class MainViewModel : ObservableObject, INotifyPropertyChanged
 {
     #region constructor
-    public MainViewModel(IApiService apiService, HolidayNameService holidayNameService, PlannerStyleService plannerStyleService)
+    public MainViewModel(IApiService apiService, HolidayNameService holidayNameService, PlannerStyleService plannerStyleService, HolidayCacheService holidayCacheService)
     {
         m_ApiService = apiService;
         m_PlannerGenerator = new PlannerGenerator(apiService, holidayNameService, plannerStyleService);
+        m_HolidayCacheService = holidayCacheService;
     }
     #endregion constructor
 
     #region fields
     private readonly IApiService m_ApiService;
     private readonly PlannerGenerator m_PlannerGenerator;
+    private readonly HolidayCacheService m_HolidayCacheService;
     #endregion fields
 
     #region properties
@@ -67,19 +70,68 @@ public partial class MainViewModel : ObservableObject, INotifyPropertyChanged
         var from = new DateOnly(Year.Value, FirstMonth.Value, 1);
         var to = from.AddMonths(NumberOfMonths.Value).AddDays(-1);
 
+        var fromYear = 2023;
+        var toYear = 2033;
+
         var countryCodes = new List<string> { "DE", "HU" };
         var allHolidays = new List<Holiday>();
 
-        for (var currentDate = from; currentDate <= to; currentDate = currentDate.AddMonths(1))
+        if (File.Exists("holiday.json"))
         {
-            foreach (string countryCode in countryCodes)
+            var holidaysFromFile = m_HolidayCacheService.ReadHolidaysFromFile().ToList();
+
+            var yearsInFile = holidaysFromFile.Select(h => h.Date.Year).Distinct().ToList();
+            var isAllYearsPresent = Enumerable.Range(from.Year, to.Year - from.Year + 1).All(year => yearsInFile.Contains(year));
+
+            if (isAllYearsPresent)
             {
-                allHolidays.AddRange(await m_ApiService.GetHolidaysAsync(currentDate.Year, countryCode));
+                allHolidays.AddRange(holidaysFromFile);
+            }
+            else
+            {
+                var existingHolidays = m_HolidayCacheService.ReadHolidaysFromFile().ToList();
+                var yearsFromUserInput = Enumerable.Range(from.Year, to.Year - from.Year + 1).ToList();
+                var missingYears = yearsFromUserInput.Except(yearsInFile).ToList();
+
+                foreach (var missingYear in missingYears)
+                {
+                    foreach (var countryCode in countryCodes)
+                    {
+                        var fetchedHolidays = await m_ApiService.GetHolidaysAsync(missingYear, countryCode);
+                        existingHolidays.AddRange(fetchedHolidays);
+                    }
+                }
+                existingHolidays.Sort((x, y) => x.Date.CompareTo(y.Date));
+
+                m_HolidayCacheService.SaveHolidaysToFile(existingHolidays);
+            }
+        }
+        else
+        {
+            var holidaysFromFile = m_HolidayCacheService.ReadHolidaysFromFile().ToList();
+            var holidaysInRange = holidaysFromFile.Where(h => h.Date.Year >= 2023 && h.Date.Year <= 2033).ToList();
+
+            if (holidaysInRange.Count != (2033 - 2023 + 1))
+            {
+                var allHolidaysList = new List<Holiday>();
+
+                for (var year = 2023; year <= 2033; year++)
+                {
+                    foreach (var countryCode in countryCodes)
+                    {
+                        var fetchedHolidays = await m_ApiService.GetHolidaysAsync(year, countryCode);
+                        allHolidaysList.AddRange(fetchedHolidays);
+                    }
+                }
+
+                holidaysFromFile.AddRange(allHolidaysList);
+                m_HolidayCacheService.SaveHolidaysToFile(holidaysFromFile);
             }
         }
 
+        await m_PlannerGenerator.GeneratePlanner(from, to, m_HolidayCacheService.ReadHolidaysFromFile(), path);
+
         // only one reference to the instance of "m_PlannerGenerator". Do we need to keep it in the class as a local variable?
-        await m_PlannerGenerator.GeneratePlanner(from, to, allHolidays, path);
         Year = null;
         FirstMonth = null;
         NumberOfMonths = null;
